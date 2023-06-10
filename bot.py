ce62c6044507b22e9896a8e59c812737e129ad81
saumyk <111625180+saumyk@users.noreply.github.com>
2023-06-10 23:07:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,11 +7,11 @@ from os import environ
 from pyrogram import Client, filters
 from pyrogram.types import *
 
-API_ID = environ.get('API_ID')
-API_HASH = environ.get('API_HASH')
-BOT_TOKEN = environ.get('BOT_TOKEN')
-API_KEY = environ.get('API_KEY')
-API_URL = environ.get('API_URL')
+API_ID = environ.get('5604474')
+API_HASH = environ.get('45425c2c16f2c177c71b5748fb87c3ad')
+BOT_TOKEN = environ.get('6024380614:AAET6k4iKbyJ0mA_XkLsxos9h_L0pQ3Ubkg')
+API_KEY = environ.get('6b9780330de6847a5750c9d404eae5e0daa5d76a')
+API_URL = environ.get('')
 
 akbotz = Client('link shortener bot',
              api_id=API_ID,
@@ -19,14 +19,14 @@ akbotz = Client('link shortener bot',
              bot_token=BOT_TOKEN,
              workers=100)
 
-print("Developer: @AKBotZ , Join & Share Channel")
+print("Developer: @ds_backup, Join & Share Channel")
 print("Bot is Started Now")
 
 @akbotz.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
     await message.reply(
         f"**Hi {message.chat.first_name}!**\n\n"
-        "I'm Link Shortener bot. Just send me link and get short link, You can also send multiple links seperated by a space or enter.\n\n**Developer:** @AKBotZ")
+        "I'm Link Shortener bot. Just send me link and get short link, You can also send multiple links seperated by a space or enter.\n\n**Developer:** @ds_backup")
 
 
 @akbotz.on_message(filters.private & filters.text & filters.incoming)
